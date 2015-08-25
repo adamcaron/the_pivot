49e89a8140d4eb4eecf7a194d8eb23ad619e73b0
Adam Caron <adamwcaron@gmail.com>
2015-08-25 16:46:45 -0600

    Add pseudo code for platform admin

---
+++ spec/features/platform_admin_manages_business_admin_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+# Platform admin manages business admin
+
+# As a platform admin, when I visit my dashboard,
+  # I should see links to businesses
+
+  # When I visit the businesses, I should see a link to manage that business' admins
+    # When I click that link,
+      # I should see all of the admins for that business / edit / delete
+      # I should see "Create New Admin"
+
+# As a business admin, when I visit my dashboard,
+  # I should be able to manage other business admins for my store
