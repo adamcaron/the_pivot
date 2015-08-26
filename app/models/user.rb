df56990ddd419cf4f7422215169d87a1f5eb7fb0
Jack Yeh <jack.yeh@gmail.com>
2015-08-26 17:09:49 -0600

    update user model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :reservations
   validates :username, presence: true,
             uniqueness: true
   enum role: %w(default admin)
