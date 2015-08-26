1dfa1918b66329a5605ef7af5991235b31db2589
David Maurer <davemaurermail@gmail.com>
2015-08-26 13:46:21 -0600

    feature test guest user sees list of lodging options

--- spec/features/guest_user_sees_list_of_lodging_options_spec.rb
+++ spec/features/guest_user_sees_list_of_lodging_options_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
-# As a guest user, when I select my lodging preferences,
-# I see a list of all the available lodging options.
+before :each do
+  visit root_path
+  select('Asia', from: 'Location')
+  page.execute_script %Q{ $('#from').trigger("focus") } # activate datetime picker
+  page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15click_on 'Search'
+  page.execute_script %Q{ $('#to').trigger("focus") } # activate datetime picker
+  page.execute_script %Q{ $("a.ui-state-default:contains('16')").trigger("click") } # click on day 15click_on 'Search'
+  click_button 'Search'
+end
 
-  # visit root
-  # select continent from 'location'
-  # fill_in checkin date with [date]
-  # fill_in checkout date with [date]
-
-  # assert page.has_content [listing for continent]
-  # assert page.has_content [listing for continent]
-  # assert page.has_content [listing for continent]
+feature 'Guest user sees a list of lodging options' do
+  scenario 'on listings index, not logged in' do
+    expect(page).to have_content('Tiki Hut 1')
+    expect(page).to have_content('Tiki Hut 2')
+    expect(page).to have_content('Tiki Hut 3')
+  end
+end
