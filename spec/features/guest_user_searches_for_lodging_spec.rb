5e6903dde97ee3bcefe43bcd71a2c8c49fab66b0
David Maurer <davemaurermail@gmail.com>
2015-08-26 13:36:06 -0600

    feature test guest user searches for lodging

--- spec/features/guest_user_searches_for_lodging_spec.rb
+++ spec/features/guest_user_searches_for_lodging_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 feature 'Guest user searches for lodging' do
-
-  # As a guest user when I visit the home page,
-  # I should be able to select lodging by checkin date, checkout date and location
-
-  # visit root
-  # select continent from 'location'
-  # fill_in checkin date with [date]
-  # fill_in checkout date with [date]
-
-  # expect current_path to eq locations_path?
+  scenario 'on home page, not logged in' do
+    visit root_path
+    select('Asia', from: 'Location')
+    page.execute_script %Q{ $('#from').trigger("focus") } # activate datetime picker
+    page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15click_on 'Search'
+    page.execute_script %Q{ $('#to').trigger("focus") } # activate datetime picker
+    page.execute_script %Q{ $("a.ui-state-default:contains('16')").trigger("click") } # click on day 15click_on 'Search'
+    click_button 'Search'
+    expect(current_path).to eq(listings_path)
+    expect(page).to have_content('Asia')
+  end
 end
