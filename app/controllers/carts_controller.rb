25cf4c0ce700878d2c5dfdf02b58b1f274be1a3d
David Maurer <davemaurermail@gmail.com>
2015-08-26 17:54:29 -0600

    changed calls to cart_listings to cart only. removed methods no longer being used

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,34 +1,18 @@
 class CartsController < ApplicationController
+  attr_reader :cart
+
   def create
     listing = Listing.find(params[:listing_id])
-    cart.add_listing(listing)
-    session[:cart] = cart.data
-    redirect_to cart_path
-  end
-
-  def index
-    @cart_trips = cart.trips
-  end
-
-  def increment
-    listing = Listing.find(params[:id])
-    cart.add_listing(listing)
-    session[:cart] = cart.data
-    redirect_to cart_path
-  end
-
-  def decrement
-    listing = Listing.find(params[:id])
-    cart.decrease_trip(listing)
-    session[:cart] = cart.data
+    cart.add_listing(listing.id)
+    session[:cart] = cart.contents
     redirect_to cart_path
   end
 
   def destroy
-    @listing = Listing.find(params[:id])
-    cart.remove_listing(@listing)
-    session[:cart] = cart.data
-    flash[:remove] = "Successfully removed #{@listing.name} from cart."
+    listing = Listing.find(params[:listing_id])
+    cart.remove_listing(listing.id)
+    session[:cart] = cart.contents
+    flash[:remove] = "Successfully removed #{listing.name} from cart."
     redirect_to cart_path
   end
 end
