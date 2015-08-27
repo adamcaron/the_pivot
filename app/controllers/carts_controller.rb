45c4bb1055b80504ff9ef3fa1b3069066ee438b5
David Maurer <davemaurermail@gmail.com>
2015-08-26 21:32:54 -0600

    removed instance variables from cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,15 +6,15 @@ class CartsController < ApplicationController
   end
 
   def create
-    @listing = Listing.find(params[:listing_id])
-    cart.add_listing(@listing.id)
+    listing = Listing.find(params[:listing_id])
+    cart.add_listing(listing.id)
     session[:cart] = cart.contents
     redirect_to cart_path
   end
 
   def destroy
-    @listing = Listing.find(params[:listing_id])
-    cart.remove_listing(@listing.id)
+    listing = Listing.find(params[:listing_id])
+    cart.remove_listing(listing.id)
     session[:cart] = cart.contents
     flash[:remove] = "Successfully removed #{listing.name} from cart."
     redirect_to cart_path
