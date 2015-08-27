1b530273fc2b9d66eedf4c5a608fb89ea98e7648
Adam Caron <adamwcaron@gmail.com>
2015-08-27 17:31:35 -0600

    Display listing info on cart page.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,11 +3,16 @@ class CartsController < ApplicationController
 
   def show
     @cart ||= Cart.new(session[:cart])
+    listing_id = @cart.contents.keys.first.to_i
+    @listing ||= Listing.find(listing_id)
+    @location ||= Location.find(@listing.location_id).continent
+    @date_range = "#{session[:dates]["to"]} - #{session[:dates]["from"]}"
   end
 
   def create
     @cart ||= Cart.new(session[:cart])
     listing = Listing.find(params[:listing_id])
+    session[:cart].clear
     cart.add_listing(listing.id)
     session[:cart] = cart.contents
     redirect_to cart_path
