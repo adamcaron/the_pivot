8dedf1d89b1199ee1d06b1895a7affe2f2218073
Adam Caron <adamwcaron@gmail.com>
2015-08-26 21:43:15 -0600

    Reformat select dropdown and test

--- spec/features/guest_user_searches_for_lodging_spec.rb
+++ spec/features/guest_user_searches_for_lodging_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Guest user searches for lodging' do
   scenario 'on home page, not logged in' do
     visit root_path
-    select('Asia', from: 'Location')
+    select('Asia', from: 'location')
     page.execute_script %Q{ $('#from').trigger("focus") } # activate datetime picker
     page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15
     page.execute_script %Q{ $('#to').trigger("focus") } # activate datetime picker
