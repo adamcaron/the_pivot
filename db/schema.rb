983e313e8431fdf52dd0b289fc60977a5bf14212
Jack Yeh <jack.yeh@gmail.com>
2015-08-29 01:07:10 +0800

    add password column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828161450) do
+ActiveRecord::Schema.define(version: 20150828170241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150828161450) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "host_id"
+    t.string   "password"
   end
 
   add_foreign_key "listings", "locations"
