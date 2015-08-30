5fa8c9c8d637a7a85156e4a2b9745af55bb6225a
David Maurer <davemaurermail@gmail.com>
2015-08-29 19:53:19 -0600

    changed session date to read from url instead of stored in session hash
    Also removed password column from user table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+
   has_secure_password
   validates :username, presence: true,
             uniqueness: true
   has_many :reservations
   enum role: %w(default admin)
-
 end
