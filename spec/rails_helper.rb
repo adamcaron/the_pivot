b3885a1d0442e38cbf5a90307f8723315cc43ac2
Jack Yeh <jack.yeh@gmail.com>
2015-09-03 11:23:05 -0600

    add capybara webkit gem configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,11 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'active_record'
 require 'helpers'
+
+connection_info = YAML.load_file('config/database.yml')['test']
+ActiveRecord::Base.establish_connection(connection_info)
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -13,4 +17,11 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.include Helpers
+
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
 end
