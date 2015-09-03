ae43c034d9860f64462170f7ddbd2761f579674f
Jack Yeh <jack.yeh@gmail.com>
2015-09-03 09:21:06 -0600

    update test to reflect pivot to a multitenancy lodging app

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,26 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
+  let(:reservation) {Reservation.create(user_id: 2)}
 
   context '#reservations' do
     it 'has a user id' do
-      reservation = create_reservation
-
       expect(reservation.user_id).to eq(2)
     end
-
-    it 'has user listings' do
-      reservation = create_reservation
-      expect(reservation.reservation_listings).to eq([])
-    end
-
-    it 'has listings' do
-      reservation = create_reservation
-      expect(reservation.listings).to eq([])
-    end
-  end
-
-  def create_reservation
-    Reservation.new(user_id: 2)
   end
 end
