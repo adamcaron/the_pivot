aa5589612aa08cd26603f6742aca8a404751cbd0
Jack Yeh <jack.yeh@gmail.com>
2015-09-02 18:40:40 -0600

    update listings#search to listings#search_results

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -26,7 +26,7 @@ class ListingsController < ApplicationController
 
   end
 
-  def search
+  def search_results
     session[:dates] = { to: params[:to], from: params[:from] }
     if !cart_listing.valid?
       flash[:invalid_search] = "Invalid search. Please try again"
