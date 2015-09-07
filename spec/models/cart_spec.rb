1b009a0a88cda36d41fba5f1944bef556a77d6f8
Jack Yeh <jack.yeh@gmail.com>
2015-09-07 15:05:11 -0600

    remove extraneous code

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  let(:cart) { Cart.create(user_id: 2) }
 
-  context '#reservations' do
-    it 'has a user id' do
-      expect(reservation.user_id).to eq(2)
-    end
-  end
 end
