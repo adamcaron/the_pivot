803d0f41d391e32348ceb244d35bb37446e9ebe8
Jack Yeh <jack.yeh@gmail.com>
2015-09-07 16:26:28 -0600

    create/pass test: unregistered user can check out

--- spec/features/guest_user_logs_in_or_creates_account_before_checkout_spec.rb
+++ spec/features/guest_user_logs_in_or_creates_account_before_checkout_spec.rb
@@ -1,86 +1,87 @@
 require 'rails_helper'
 
 feature 'User checks out' do
-  before :each do
-    location = Location.create!(continent: 'Africa')
-    Listing.create!(name: 'testing hut', location_id: location.id, cost: 99)
-    Listing.create!(name: 'testing hut2', location_id: location.id, cost: 129)
+  describe 'views listings' do
 
-    visit root_path
-    select('Africa', from: 'location')
-    fill_in 'from', with: '09/24/2015'
-    fill_in 'to', with: '09/30/2015'
+    before :each do
+      location = Location.create!(continent: 'Africa')
+      listing1 = Listing.create!(name: 'testing hut', location_id: location.id, cost: 99)
+      listing2 = Listing.create!(name: 'testing hut2', location_id: location.id, cost: 129)
+      Role.create!(title: "registered_user")
 
-    click_button 'Search'
+      visit root_path
+      page.select('Africa', from: 'location')
+      fill_in 'from', with: '09/29/2015'
+      fill_in 'to', with: '09/30/2015'
+      click_button 'Search'
 
-    within(page.first('div#whole-card')) do
+      expect(current_path).to eq(search_results_path)
+
+      within(page.first('div.whole-card')) do
       title = find('h3')
       expect(title.text).to eq('testing hut')
 
       click_link("View Listing")
+      end
     end
-  end
 
-  xscenario 'unregistered user' do
-    expect(current_path).to eq(cart_path)
-    click_button 'Checkout'
-    expect(current_path).to eq(login_path)
+    scenario 'unregistered user' do
+      expect(page).to have_content('testing hut')
+      click_link("Book Listing")
+      click_link('Submit Payment')
+      expect(current_path).to eq(login_path)
 
-    click_button 'Create Account'
+      click_link('Register')
 
-    expect(current_path).to eq(user_new_path)
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content('Register')
 
-    expect(page).to have_content('Register')
+      fill_in 'Username', with: "johnny"
+      fill_in 'Password', with: "password"
 
-    fill_in 'Username', with: "dave"
-    fill_in 'Password', with: "password"
+      click_button 'Create User'
+      click_link('Submit Payment')
 
-    click_button 'Create Account'
+      expect(current_path).to eq(reservations_path)
+      expect(page).to have_content('Reservation was successfully placed!')
 
-    visit cart_path
+      user = User.find_by(username: 'johnny')
+      expect(user.reservations.count).to eq(1)
+    end
 
-    click_button 'Checkout'
+    xscenario 'registered user, not logged in' do
+      user = User.create(username: 'dave', password: 'password')
 
-    expect(current_path).to eq(reservations_path)
-    expect(page).to have_content('Reservations')
+      expect(current_path).to eq(cart_path)
+      click_button 'Checkout'
+      expect(current_path).to eq(login_path)
 
-    user = User.find_by(username: 'dave')
+      fill_in 'Username', with: user.username
+      fill_in 'Password', with: user.password
 
-    expect(user.orders.count).to eq(1)
-  end
+      click_button 'Login'
 
-  xscenario 'registered user, not logged in' do
-    user = User.create(username: 'dave', password: 'password')
+      visit cart_path
 
-    expect(current_path).to eq(cart_path)
-    click_button 'Checkout'
-    expect(current_path).to eq(login_path)
+      expect(click_button 'Checkout').to change(Reservation, :count).by(1)
 
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
+      expect(current_path).to eq(reservations_path)
+      expect(page).to have_content('Reservations')
+    end
 
-    click_button 'Login'
+    xscenario 'registered user, already logged in' do
+      user = User.create(username: 'dave', password: 'password')
+      visit login_path
+      fill_in 'Username', with: user.username
+      fill_in 'Password', with: user.password
+      click_button 'Login'
 
-    visit cart_path
+      visit cart_path
 
-    expect(click_button 'Checkout').to change(Reservation, :count).by(1)
+      expect(click_button 'Checkout').to change(Reservation, :count).by(1)
 
-    expect(current_path).to eq(reservations_path)
-    expect(page).to have_content('Reservations')
-  end
-
-  xscenario 'registered user, already logged in' do
-    user = User.create(username: 'dave', password: 'password')
-    visit login_path
-    fill_in 'Username', with: user.username
-    fill_in 'Password', with: user.password
-    click_button 'Login'
-
-    visit cart_path
-
-    expect(click_button 'Checkout').to change(Reservation, :count).by(1)
-
-    expect(current_path).to eq(reservations_path)
-    expect(page).to have_content('Reservations')
+      expect(current_path).to eq(reservations_path)
+      expect(page).to have_content('Reservations')
+    end
   end
 end
