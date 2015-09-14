d4fc58f07290e4d28280f9987977eaca60709a57
Jack Yeh <jack.yeh@gmail.com>
2015-09-13 23:39:00 -0600

    pass test: guest user sees listings from search results

--- spec/features/guest_user_sees_list_of_lodging_options_spec.rb
+++ spec/features/guest_user_sees_list_of_lodging_options_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 feature 'Guest user sees a list of lodging options' do
   before :each do
-    create_locations
-    create_listings
-
+    location = Location.create!(continent: 'Africa')
+    Listing.create!(name: 'Tiki Hut 0', location_id: location.id, cost: 19)
+    Listing.create!(name: 'Tiki Hut 1', location_id: location.id, cost: 29)
+    Listing.create!(name: 'Tiki Hut 2', location_id: location.id, cost: 39)
 
     visit root_path
-    select('Asia', from: 'Location')
-    page.execute_script %Q{ $('#from').trigger("focus") } # activate datetime picker
-    page.execute_script %Q{ $("a.ui-state-default:contains('15')").trigger("click") } # click on day 15click_on 'Search'
-    page.execute_script %Q{ $('#to').trigger("focus") } # activate datetime picker
-    page.execute_script %Q{ $("a.ui-state-default:contains('16')").trigger("click") } # click on day 15click_on 'Search'
+    page.select('Africa', from: 'location')
+    fill_in 'from', with: '09/29/2015'
+    fill_in 'to', with: '09/30/2015'
     click_button 'Search'
+
   end
 
   scenario 'on listings index, not logged in' do
+    expect(current_path).to eq(search_results_path)
     expect(page).to have_content('Tiki Hut 0')
     expect(page).to have_content('Tiki Hut 1')
     expect(page).to have_content('Tiki Hut 2')
