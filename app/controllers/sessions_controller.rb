3f691a71662bed3712527ec515049299ba31b3c5
Adam Caron <adamwcaron@gmail.com>
2015-10-07 09:25:49 -0600

    Check if cart has contents after user logs in
    redirect user to his/her cart if it has contents;
    if cart's empty, redirect to homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
@@ -7,8 +6,8 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      flash[:notice] = "Welcome back #{@user.username}!"
-      redirect_to new_reservation_path
+      flash[:notice]    = "Welcome back #{@user.username}!"
+      send_to_checkout_or_homepage
     else
       flash[:error] = "Invalid login credentials"
       render :new
@@ -18,5 +17,13 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  def send_to_checkout_or_homepage
+    if params[:listing_id]
+      redirect_to new_reservation_path
+    else
+      redirect_to root_path
+    end
   end
 end
