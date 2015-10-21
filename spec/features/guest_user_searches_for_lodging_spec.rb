51c78c5742e7c366385ae3ac856c7b869fe0de76
Jack Yeh <jack.yeh@gmail.com>
2015-10-20 19:04:07 -0600

    create/pass test: guest users searches for lodging without all fields filled in

--- spec/features/guest_user_searches_for_lodging_spec.rb
+++ spec/features/guest_user_searches_for_lodging_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'Guest user searches for lodging' do
-  scenario 'on home page, not logged in' do
+  scenario 'on home page, not logged in and performs valid search' do
 
     location = Location.create!(continent: 'Africa')
     listing1 = Listing.create!(name: 'testing hut', location_id: location.id, cost: 99)
@@ -16,5 +16,19 @@ feature 'Guest user searches for lodging' do
     expect(current_path).to eq(search_results_path)
     expect(page).to have_content('testing hut')
     expect(page).to have_content('testing hut2')
+  end
+
+  scenario 'on home page, not logged in searches without valid info such as date' do
+
+    location = Location.create!(continent: 'Africa')
+    listing1 = Listing.create!(name: 'testing hut', location_id: location.id, cost: 99)
+    listing2 = Listing.create!(name: 'testing hut2', location_id: location.id, cost: 129)
+
+    visit root_path
+    page.select('Africa', from: 'location')
+    fill_in 'check_out',  with: '09/30/2015'
+    click_button 'Search'
+
+    expect(current_path).to eq(root_path)
   end
 end
